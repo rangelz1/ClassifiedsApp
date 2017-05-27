1381f5615b940a1f180caad32a50451b8ab35222
rangelz1 <rangelz187@gmail.com>
2017-05-27 15:25:04 -0700

    Angular material directories added

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -6,6 +6,13 @@
 	.module("ngClassifieds")
 	.controller("classifiedsCtrl", function($scope) {
 
-		$scope.name = "Carlos";
+
+
+		$scope.classified = {
+			title:"First Item",
+			price: "$1,000.00",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, ipsam, velit. Maxime laborum, hic blanditiis aliquam praesentium. Fugiat hic laboriosam corrupti minima quidem delectus dolor doloremque, est enim, magni corporis!"
+		}
+
 	});
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-angular.module("ngClassifieds", []);
+angular
+	.module("ngClassifieds", ["ngMaterial"])
+	.config(function($mdThemingProvider){
+
+		$mdThemingProvider.theme('default')
+			.primaryPalette('green')
+			.backgroundPalette('teal')
+			.accentPalette('orange');
+
+	})
+	.directive("helloWorld", function() {
+		return {
+			template: "{{ message }}"
+		}
+	});

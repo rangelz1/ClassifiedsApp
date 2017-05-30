7786bf6041d538a308a77c01919723bea4da6a21
rangelz1 <rangelz187@gmail.com>
2017-05-30 07:09:09 -0700

    Admin functionality is working

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,7 +4,7 @@
 
 	angular
 	.module("ngClassifieds")
-	.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+	.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
 		classifiedsFactory.getClassifieds().then(function(classifieds) {
 			$scope.classifieds = classifieds.data;
@@ -30,14 +30,47 @@
 				$scope.classifieds.push(classified);
 				$scope.classified = {};
 				$scope.closeSidebar();
-				$mdToast.show(
+				showToast("Classified Saved!")
+			}
+			
+		}
+		$scope.editClassified = function(classified) {
+			$scope.editing = true;
+			$scope.openSidebar();
+			$scope.classified = classified;
+		}
+
+		$scope.saveEdit = function() {
+			$scope.editing = false;
+			$scope.classified = {};
+			$scope.closeSidebar();
+			showToast("Edit Classified Saved!")
+		}
+
+		$scope.deleteClassified = function(event, classified) {
+			var confirm = $mdDialog.confirm()
+				.title('Are you sure you want to delete ' + classified.title+'?')
+				.ok('Yes')
+				.cancel('No')
+				.targetEvent(event);
+			$mdDialog.show(confirm).then(function() {
+				var index = $scope.classifieds.indexOf(classified);
+				$scope.classifieds.splice(index, 1);
+			}, function() {
+				
+			})
+
+			
+			
+		}
+
+		function showToast(message) {
+			$mdToast.show(
 					$mdToast.simple()
-					.content("Classified Saved!")
+					.content(message)
 					.position('top, right')
 					.hideDelay(3000)
 					);
-			}
-			
 		}
 
 	});
